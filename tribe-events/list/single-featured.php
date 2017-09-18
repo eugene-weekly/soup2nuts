310e51ef7106e79b60b36ea864c48c0745b000e1
Nate Bedortha <nate@oknoway.com>
2017-09-17 21:44:30 -0700

    Events cleanup

--- tribe-events/list/single-featured.php
+++ tribe-events/list/single-featured.php
@@ -10,7 +10,7 @@
  *
  */
 if ( ! defined( 'ABSPATH' ) ) {
-	die( '-1' );
+  die( '-1' );
 }
 
 // Setup an array of venue details for use later in the template
@@ -22,55 +22,62 @@ $has_venue_address = ( ! empty( $venue_details['address'] ) ) ? ' location' : ''
 // Organizer
 $organizer = tribe_get_organizer();
 ?>
-<!-- Event Cost -->
-<?php if ( tribe_get_cost() ) : ?>
-	<div class="tribe-events-event-cost">
-		<span><?php echo tribe_get_cost( null, true ); ?></span>
-	</div>
-<?php endif; ?>
+<header class="event-header">
+  <!-- Event Title -->
+  <?php do_action('tribe_events_before_the_event_title') ?>
+  <h2 class="tribe-events-list-event-title">
+    <a class="tribe-event-url" href="<?php echo esc_url(tribe_get_event_link()); ?>" title="<?php the_title_attribute() ?>" rel="bookmark">
+    <?php the_title() ?>
+    </a>
+  </h2>
+  <?php do_action('tribe_events_after_the_event_title') ?>
 
-<!-- Event Title -->
-<?php do_action( 'tribe_events_before_the_event_title' ) ?>
-<h2 class="tribe-events-list-event-title">
-	<a class="tribe-event-url" href="<?php echo esc_url( tribe_get_event_link() ); ?>" title="<?php the_title_attribute() ?>" rel="bookmark">
-		<?php the_title() ?>
-	</a>
-</h2>
-<?php do_action( 'tribe_events_after_the_event_title' ) ?>
+  <!-- Event Meta -->
+  <?php do_action('tribe_events_before_the_meta') ?>
+  <div class="tribe-events-event-meta">
+    <div class="author <?php echo esc_attr($has_venue_address); ?>">
 
-<!-- Event Meta -->
-<?php do_action( 'tribe_events_before_the_meta' ) ?>
-<div class="tribe-events-event-meta">
-	<div class="author <?php echo esc_attr( $has_venue_address ); ?>">
+      <!-- Schedule & Recurrence Details -->
+      <div class="tribe-event-schedule-details">
+        <?php echo tribe_events_event_schedule_details() ?>
+      </div>
 
-		<!-- Schedule & Recurrence Details -->
-		<div class="tribe-event-schedule-details">
-			<?php echo tribe_events_event_schedule_details() ?>
-		</div>
+      <?php if ($venue_details) : ?>
+      <!-- Venue Display Info -->
+      <div class="tribe-events-venue-details">
+        <?php echo implode(', ', $venue_details); ?>
+        <?php if (tribe_get_map_link()) {
+          echo tribe_get_map_link_html();
+        } ?>
+      </div>
+      <!-- .tribe-events-venue-details -->
+      <?php endif; ?>
 
-		<?php if ( $venue_details ) : ?>
-			<!-- Venue Display Info -->
-			<div class="tribe-events-venue-details">
-				<?php echo implode( ', ', $venue_details ); ?>
-				<?php
-				if ( tribe_get_map_link() ) {
-					echo tribe_get_map_link_html();
-				}
-				?>
-			</div> <!-- .tribe-events-venue-details -->
-		<?php endif; ?>
+    </div>
 
-	</div>
-</div><!-- .tribe-events-event-meta -->
-<?php do_action( 'tribe_events_after_the_meta' ) ?>
+    <!-- Event Cost -->
+    <?php if (tribe_get_cost()) : ?>
+    <div class="tribe-events-event-cost">
+      <span><?php echo tribe_get_cost(null, true); ?></span>
+    </div>
+    <?php endif; ?>
 
+  </div>
+  <!-- .tribe-events-event-meta -->
+  <?php do_action('tribe_events_after_the_meta') ?>
+
+</header>
 <!-- Event Content -->
 <?php do_action( 'tribe_events_before_the_content' ) ?>
 <div class="tribe-events-list-event-description tribe-events-content">
-	<?php echo tribe_events_get_the_excerpt( null, wp_kses_allowed_html( 'post' ) ); ?>
-	<a href="<?php echo esc_url( tribe_get_event_link() ); ?>" class="tribe-events-read-more" rel="bookmark"><?php esc_html_e( 'Find out more', 'the-events-calendar' ) ?> &raquo;</a>
+  <?php echo tribe_events_get_the_excerpt( null, wp_kses_allowed_html( 'post' ) ); ?>
+  <a href="<?php echo esc_url( tribe_get_event_link() ); ?>" class="tribe-events-read-more" rel="bookmark"><?php esc_html_e( 'Find out more', 'the-events-calendar' ) ?> &raquo;</a>
 </div><!-- .tribe-events-list-event-description -->
 <?php
 do_action( 'tribe_events_after_the_content' ); ?>
 
-<?php echo tribe_event_featured_image( null, 'large' );
+<?php echo tribe_event_featured_image( null, 'large' ); ?>
+
+<footer class="entry-footer">
+  <?php soup2nuts_entry_footer(); ?>
+</footer>
