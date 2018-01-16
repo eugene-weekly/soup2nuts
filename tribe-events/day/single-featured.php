03770e1acd4a00458d22005c93cf2f141aba3736
Nate Bedortha <nate@oknoway.com>
2018-01-15 16:33:37 -0800

    Events/Day: Featured Event layout fix

--- tribe-events/day/single-featured.php
+++ tribe-events/day/single-featured.php
@@ -9,62 +9,75 @@
  *
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	die( '-1' );
-}
+ if ( ! defined( 'ABSPATH' ) ) {
+   die( '-1' );
+ }
 
-$venue_details = tribe_get_venue_details();
+ // Setup an array of venue details for use later in the template
+ $venue_details = tribe_get_venue_details();
 
-// Venue microformats
-$has_venue = ( $venue_details ) ? ' vcard' : '';
-$has_venue_address = ( ! empty( $venue_details['address'] ) ) ? ' location' : '';
+ // Venue
+ $has_venue_address = ( ! empty( $venue_details['address'] ) ) ? ' location' : '';
 
-echo tribe_event_featured_image( null, 'large' );
-?>
-<h1>DAY FEATURED</h1>
-<!-- Event Title -->
-<?php do_action( 'tribe_events_before_the_event_title' ) ?>
-<h2 class="tribe-events-list-event-title summary">
-	<a class="url" href="<?php echo esc_url( tribe_get_event_link() ); ?>" title="<?php the_title_attribute() ?>" rel="bookmark">
-		<?php the_title() ?>
-	</a>
-</h2>
-<?php do_action( 'tribe_events_after_the_event_title' ) ?>
+ // Organizer
+ $organizer = tribe_get_organizer();
+ ?>
+ <header class="event-header">
+   <!-- Event Title -->
+   <?php do_action('tribe_events_before_the_event_title') ?>
+   <h2 class="tribe-events-list-event-title">
+     <a class="tribe-event-url" href="<?php echo esc_url(tribe_get_event_link()); ?>" title="<?php the_title_attribute() ?>" rel="bookmark">
+     <?php the_title() ?>
+     </a>
+   </h2>
+   <?php do_action('tribe_events_after_the_event_title') ?>
 
-<!-- Event Meta -->
-<?php do_action( 'tribe_events_before_the_meta' ) ?>
-<div class="tribe-events-event-meta <?php echo esc_attr( $has_venue . $has_venue_address ); ?>">
+   <!-- Event Meta -->
+   <?php do_action('tribe_events_before_the_meta') ?>
+   <div class="tribe-events-event-meta">
+     <div class="author <?php echo esc_attr($has_venue_address); ?>">
 
-	<!-- Schedule & Recurrence Details -->
-	<div class="tribe-updated published time-details">
-		<?php echo tribe_events_event_schedule_details(); ?>
-	</div>
+       <!-- Schedule & Recurrence Details -->
+       <div class="tribe-event-schedule-details">
+         <?php echo tribe_events_event_schedule_details() ?>
+       </div>
 
-	<?php if ( $venue_details ) : ?>
-		<!-- Venue Display Info -->
-		<div class="tribe-events-venue-details">
-			<?php echo implode( ', ', $venue_details ); ?>
-		</div> <!-- .tribe-events-venue-details -->
-	<?php endif; ?>
+       <?php if ($venue_details) : ?>
+       <!-- Venue Display Info -->
+       <div class="tribe-events-venue-details">
+         <?php echo implode(', ', $venue_details); ?>
+         <?php if (tribe_get_map_link()) {
+           echo tribe_get_map_link_html();
+         } ?>
+       </div>
+       <!-- .tribe-events-venue-details -->
+       <?php endif; ?>
 
-</div><!-- .tribe-events-event-meta -->
-<?php do_action( 'tribe_events_after_the_meta' ) ?>
+     </div>
 
-<?php if ( tribe_get_cost() ) : ?>
-	<div class="tribe-events-event-cost">
-		<span class="ticket-cost"><?php echo tribe_get_cost( null, true ); ?></span>
-		<?php
-		/** This action is documented in the-events-calendar/src/views/list/single-event.php */
-		do_action( 'tribe_events_inside_cost' )
-		?>
-	</div>
-<?php endif; ?>
+     <!-- Event Cost -->
+     <?php if (tribe_get_cost()) : ?>
+     <div class="tribe-events-event-cost">
+       <span><?php echo tribe_get_cost(null, true); ?></span>
+     </div>
+     <?php endif; ?>
 
-<!-- Event Content -->
-<?php do_action( 'tribe_events_before_the_content' ) ?>
-<div class="tribe-events-list-event-description tribe-events-content description entry-summary">
-	<?php echo tribe_events_get_the_excerpt(); ?>
-	<a href="<?php echo esc_url( tribe_get_event_link() ); ?>" class="tribe-events-read-more" rel="bookmark"><?php esc_html_e( 'Find out more', 'the-events-calendar' ) ?> &raquo;</a>
-</div><!-- .tribe-events-list-event-description -->
-<?php
-do_action( 'tribe_events_after_the_content' );
+   </div>
+   <!-- .tribe-events-event-meta -->
+   <?php do_action('tribe_events_after_the_meta') ?>
+
+ </header>
+ <!-- Event Content -->
+ <?php do_action( 'tribe_events_before_the_content' ) ?>
+ <div class="tribe-events-list-event-description tribe-events-content">
+   <?php echo tribe_events_get_the_excerpt( null, wp_kses_allowed_html( 'post' ) ); ?>
+   <a href="<?php echo esc_url( tribe_get_event_link() ); ?>" class="tribe-events-read-more" rel="bookmark"><?php esc_html_e( 'Find out more', 'the-events-calendar' ) ?> &raquo;</a>
+ </div><!-- .tribe-events-list-event-description -->
+ <?php
+ do_action( 'tribe_events_after_the_content' ); ?>
+
+ <?php echo tribe_event_featured_image( null, 'large' ); ?>
+
+ <footer class="entry-footer">
+   <?php soup2nuts_entry_footer(); ?>
+ </footer>
